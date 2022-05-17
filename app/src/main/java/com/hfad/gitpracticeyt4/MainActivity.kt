ab559525762ca7756930dc103d3cf8b644944a31
nicholaus michael <nicholausmichael51@gmail.com>
2022-05-17 06:56:52 +0100

    Revert "This is our third commit"
    This reverts commit e418730a723b5128cd9138389f1a23f5725f1c7f.

--- app/src/main/java/com/hfad/gitpracticeyt4/MainActivity.kt
+++ app/src/main/java/com/hfad/gitpracticeyt4/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("THIS IS CHANGED IN OUR SECOND COMMIT")
-        today is a good day
     }
 }
